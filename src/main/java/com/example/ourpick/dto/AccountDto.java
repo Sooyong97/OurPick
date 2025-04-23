02dd3455617f06e0b102b66a00b930b21b49bdad
Sooyong Yoon <ybh6703@naver.com>
2025-04-23 22:03:40 +0900

    feat: Account Dto 기능 구현

--- src/main/java/com/example/ourpick/dto/AccountDto.java
+++ src/main/java/com/example/ourpick/dto/AccountDto.java
@@ -1,4 +1,25 @@
 package com.example.ourpick.dto;
 
+import com.example.ourpick.domain.Account;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AccountDto {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private boolean isAdmin;
+
+    public AccountDto(Account account) {
+        this.id = account.getId();
+        this.name = account.getName();
+        this.email = account.getEmail();
+        this.password = account.getPassword();
+        this.isAdmin = account.isAdmin();
+    }
 }
