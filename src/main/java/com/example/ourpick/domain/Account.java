b0e100d268359c9d30d989fc2f8cf0cd63e9ad9d
Sooyong Yoon <ybh6703@naver.com>
2025-04-23 18:01:40 +0900

    feat: Account 도메인 생성

---
+++ src/main/java/com/example/ourpick/domain/Account.java
@@ -0,0 +1,10 @@
+package com.example.ourpick.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+public class Account {
+}
