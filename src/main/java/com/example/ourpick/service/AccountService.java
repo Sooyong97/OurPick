0c8b6fd8d8efc7ccb9db6e754fa6006ef10d6d88
Sooyong Yoon <ybh6703@naver.com>
2025-04-23 23:20:00 +0900

    feat: 회원가입 서비스 구현

--- src/main/java/com/example/ourpick/service/AccountService.java
+++ src/main/java/com/example/ourpick/service/AccountService.java
@@ -1,4 +1,32 @@
 package com.example.ourpick.service;
 
+import com.example.ourpick.domain.Account;
+import com.example.ourpick.dto.AccountDto;
+import com.example.ourpick.repository.AccountRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Service
+@RequiredArgsConstructor
 public class AccountService {
+    private final AccountRepository accountRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 회원가입
+    public void register(AccountDto dto) {
+        if (accountRepository.existsByEmail(dto.getId())) {
+            throw new IllegalArgumentException("이미 가입된 아이디입니다.");
+        }
+
+        Account account = Account.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build();
+
+        accountRepository.save(account);
+    }
 }
