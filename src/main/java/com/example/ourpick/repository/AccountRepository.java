915e4c52ce5cb5ff8bbb3fd57d21e2cb416cfa86
Sooyong Yoon <ybh6703@naver.com>
2025-04-23 22:00:34 +0900

    feat: Account Repository 기능 구현

--- src/main/java/com/example/ourpick/repository/AccountRepository.java
+++ src/main/java/com/example/ourpick/repository/AccountRepository.java
@@ -1,4 +1,15 @@
 package com.example.ourpick.repository;
 
-public interface AccountRepository {
+import com.example.ourpick.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, String> {
+
+    Optional<Account> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsById(String id);
 }
