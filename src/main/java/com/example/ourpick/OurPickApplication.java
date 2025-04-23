a2e21532c1d9b6e04daa91e2fb1483f9b1314566
Sooyong Yoon <ybh6703@naver.com>
2025-04-23 17:01:24 +0900

    Initation Project

---
+++ src/main/java/com/example/ourpick/OurPickApplication.java
@@ -0,0 +1,13 @@
+package com.example.ourpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ourpick/OurPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ourpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
