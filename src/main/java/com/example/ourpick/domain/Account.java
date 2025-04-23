09d9131e7a46080f274e0c80cdb0675ab0ad8d47
Sooyong Yoon <ybh6703@naver.com>
2025-04-23 18:50:00 +0900

    feat: Account 도메인 구현

--- src/main/java/com/example/ourpick/domain/Account.java
+++ src/main/java/com/example/ourpick/domain/Account.java
@@ -6,5 +6,30 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
 
+@Entity
+@Table(name = "accounts")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Account {
+
+    @Id
+    @Column(length = 16, nullable = false)
+    private String id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 254, nullable = false)
+    private String email;
+
+    @Setter
+    @Column(length = 200, nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean isAdmin = false;
+
 }
