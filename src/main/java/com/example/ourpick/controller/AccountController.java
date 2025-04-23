d95153b73ac4796fddefbdf0e7ab25ab781973a7
Sooyong Yoon <ybh6703@naver.com>
2025-04-23 23:24:01 +0900

    feat: 회원가입 컨트롤러 구현

--- src/main/java/com/example/ourpick/controller/AccountController.java
+++ src/main/java/com/example/ourpick/controller/AccountController.java
@@ -1,4 +1,23 @@
 package com.example.ourpick.controller;
 
+import com.example.ourpick.dto.AccountDto;
+import com.example.ourpick.service.AccountService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/accounts")
+@CrossOrigin(origins = "*")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody AccountDto accountDto) {
+        accountService.register(accountDto);
+        return ResponseEntity.ok("회원가입 성공");
+    }
 }
